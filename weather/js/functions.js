d843a3b5e90d6e9c8338bde5509626382ce7808f
LystraZibiah <zib15001@byui.edu>
2019-06-21 10:58:44 -0600

    new

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -174,24 +174,3 @@ function buildHourlyData(nextHour,hourlyTemps) {
    // Get the next hour based on the current time
 let date = new Date(); 
 let nextHour = date.getHours() + 1;
-fetch(URL)
- .then(function(response) {
-   return response.json();
- })
- .then(function(data){
-   ... do something with the JavaScript object ...
- })
- .catch( ... do something if an error occurred)
- fetch(URL)
- .then(function(response) {
-   if(response.ok){
-     return response.json();
-   }
-   throw new ERROR('Network response was not OK.');
- })
- .then(function(data){
-   ... do something with the JavaScript object ...
- })
- .catch(function(error){
-console.log('There was a fetch problem: ', error.message);
- })
